a96d27e11a049a989b246e871deaab02c1f38a8f
sksami3 <sksami4456@gmail.com>
2020-10-23 22:33:26 +0600

    Add project files.

---
+++ AspDotNetCoreCustomAuthenticationWIthIdentiy.Infrustructure/AuthDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AspDotNetCoreCustomAuthenticationWIthIdentiy.Infrustructure
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        IConfigurationRoot configuration = new ConfigurationBuilder()
+        //           .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        //        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        //        optionsBuilder.UseSqlServer(connectionString);
+
+        //        //optionsBuilder.UseLazyLoadingProxies();
+        //    }
+        //}
+    }
+}
---
+++ AspDotNetCoreCustomAuthenticationWIthIdentiy/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspDotNetCoreCustomAuthenticationWIthIdentiy.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspDotNetCoreCustomAuthenticationWIthIdentiy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserManager<IdentityUser> UserManager { get; }
+        public SignInManager<IdentityUser> SignInManager { get; }
+
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IQueryable<IdentityUser> Get()
+        {
+            return UserManager.Users;
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<bool> Post(RegisterVM registerVM)
+        {
+            if (registerVM != null)
+            {
+                var user = new IdentityUser { Email = registerVM.Email, UserName = registerVM.UserName };
+                var result = await UserManager.CreateAsync(user, registerVM.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false);
+                    return true;
+                }
+                else
+                    //result.Errors;
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
